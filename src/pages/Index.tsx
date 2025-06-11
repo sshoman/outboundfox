f9eb051a0d95bc954c0bb37417429d34e8d826ea
Shoman <shoman@outlook.co.il>
2025-06-11 10:32:44 +0300

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -132,7 +132,7 @@ const Index = () => {
   title=""
   className="w-full h-full"
   frameBorder="0"
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
   allowFullScreen
 ></iframe>
             </div>
